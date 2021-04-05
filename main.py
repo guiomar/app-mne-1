291ea0529c7d6d1e11a8181b81a85662b7775766
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-04-05 17:38:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,40 @@ mnest_path = '/Users/guiomar/Documents/GitHub/mne-bids-pipeline'
 
 # Populate mne_config.py file with brainlife config.json
 # - load inputs from config.json
-#with open(__location__+'/config.json') as config_json:
-#    config = json.load(config_json)
+with open(__location__+'/config.json') as config_json:
+    config = json.load(config_json)
 
-#bids_root = str(config['fif'])
+#study_name = 'ds000246'
+bids_root = str(config['fif']) # '/Users/guiomar/Projects/ds000246'
+deriv_root = 'out_dir'
+#runs = ['01']
+l_freq = .3
+h_freq = 100.
+decim = 10 #4
+#subjects = ['0001']
+ch_types = ['meg']
+reject = dict(mag=4e-12, eog=250e-6)
+#conditions = ['standard', 'deviant', 'button']
+#contrasts = [('deviant', 'standard')]
+decode = True
+#daysback = -365 * 110
+on_error = 'debug'
 
+# Create new MNE config .py file
+
+fname = 'mne_config1.py'
+
+with open(fname, 'w') as f:
+    #f.write('bids_root = '  + repr(bids_root)+'\n')
+    f.write("bids_root = '{}'".format(bids_root)+'\n')
+    f.write("deriv_root = '{}'".format(deriv_root)+'\n')
+    f.write('l_freq = {}'.format(l_freq)+'\n')
+    f.write('h_freq = {}'.format(h_freq)+'\n')
+    f.write('decim = {}'.format(decim)+'\n')
+    f.write('ch_types = {}'.format(ch_types)+'\n')
+
+    f.close()
 
 # Run mne-study-template python script
-os.system( mnest_path + '/run.py --config=' + __location__+'/mne_config.py \
-    --steps=preprocessing,sensor,report')
+#os.system( mnest_path + '/run.py --config=' + __location__+'/mne_config.py \
+#    --steps=preprocessing,sensor,report')
