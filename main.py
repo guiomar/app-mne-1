e6c01b521da51c7228c99fc7aa5a99def3fe4b81
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-04-08 19:05:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # Path to mne-study-template 
-mnest_path = '/Users/guiomar/Documents/GitHub/mne-bids-pipeline'
-
+#mnest_path = '/Users/guiomar/Documents/GitHub/mne-bids-pipeline'
+mnest_path = '/mne-bids-pipeline'
 
 # Populate mne_config.py file with brainlife config.json
 # - load inputs from config.json
@@ -29,17 +29,17 @@ with open(__location__+'/config.json') as config_json:
 #study_name = 'ds000246'
 bids_root = str(config['fif']) # '/Users/guiomar/Projects/ds000246'
 deriv_root = 'out_dir'
-#runs = ['01']
+subjects = ['0001']
+runs = ['01']
 l_freq = .3
 h_freq = 100.
 decim = 10 #4
-#subjects = ['0001']
 ch_types = ['meg']
 reject = dict(mag=4e-12, eog=250e-6)
-#conditions = ['standard', 'deviant', 'button']
-#contrasts = [('deviant', 'standard')]
+conditions = ['standard', 'deviant', 'button']
+contrasts = [('deviant', 'standard')]
 decode = True
-#daysback = -365 * 110
+##daysback = -365 * 110
 on_error = 'debug'
 
 # Create new MNE config .py file
@@ -50,13 +50,19 @@ with open(fname, 'w') as f:
     #f.write('bids_root = '  + repr(bids_root)+'\n')
     f.write("bids_root = '{}'".format(bids_root)+'\n')
     f.write("deriv_root = '{}'".format(deriv_root)+'\n')
+    f.write('subjects = {}'.format(subjects)+'\n')
+    f.write('runs = {}'.format(runs)+'\n')
     f.write('l_freq = {}'.format(l_freq)+'\n')
     f.write('h_freq = {}'.format(h_freq)+'\n')
     f.write('decim = {}'.format(decim)+'\n')
     f.write('ch_types = {}'.format(ch_types)+'\n')
+    f.write('reject = {}'.format(reject)+'\n')
+    f.write('conditions = {}'.format(conditions)+'\n')
+    f.write('contrasts = {}'.format(contrasts)+'\n')
+    f.write('decode = {}'.format(decode)+'\n')
 
     f.close()
 
 # Run mne-study-template python script
-#os.system( mnest_path + '/run.py --config=' + __location__+'/mne_config.py \
-#    --steps=preprocessing,sensor,report')
+os.system( mnest_path + '/run.py --config=' + __location__+'/mne_config.py \
+    --steps=preprocessing,sensor,report')
